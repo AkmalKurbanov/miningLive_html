617a26f8f6cf4031c4c80e5fd0e53df1d42b183e
Kurbanov Akmal <akmal.k@adver.kg>
2019-01-29 19:15:43 +0600

    last

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,44 +1,10 @@
 $(document).ready(function () {
 
-
-
-
-
-
-  // hamburger
-  $('.hamburger').click(function () {
-    $(this).toggleClass('openClose');
-    $('.mnu-js').toggleClass('mobileMnu-js');
-    $('.hamburger__line').toggleClass('lineBg');
-  });
-
-  $('.mnu-js').click(function () {
-    $(this).removeClass('mobileMnu-js');
-    $('.hamburger').removeClass('openClose');
-    $('.hamburger__line').removeClass('lineBg');
-  });
-  // hamburger end
-
-
-
-  // scroll button
-  function scrollToSection(event) {
-    event.preventDefault();
-    var $section = $($(this).attr('href'));
-    $('html, body').animate({
-      scrollTop: $section.offset().top
-    }, 700);
-  }
-  $('[data-scroll]').on('click', scrollToSection);
-  // scroll button end
-
-
-
-
   //   slider
 
   // news slider
   var swiper = new Swiper('.sliderNews-js', {
+    slidesPerView: 3,
     loop: true,
     pagination: {
       el: '.swiper-pagination',
@@ -53,6 +19,20 @@ $(document).ready(function () {
       delay: 5000,
       disableOnInteraction: false,
     },
+    breakpoints: {
+      1199: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      991: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      767: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+    }
   });
   // news slider end
 
@@ -87,6 +67,41 @@ $(document).ready(function () {
   //  projects slider end
 
   //   slider end
+
+
+
+
+  // hamburger
+  $('.hamburger').click(function () {
+    $(this).toggleClass('openClose');
+    $('.mnu-js').toggleClass('mobileMnu-js');
+    $('.hamburger__line').toggleClass('lineBg');
+  });
+
+  $('.mnu-js').click(function () {
+    $(this).removeClass('mobileMnu-js');
+    $('.hamburger').removeClass('openClose');
+    $('.hamburger__line').removeClass('lineBg');
+  });
+  // hamburger end
+
+
+
+  // scroll button
+  function scrollToSection(event) {
+    event.preventDefault();
+    var $section = $($(this).attr('href'));
+    $('html, body').animate({
+      scrollTop: $section.offset().top
+    }, 700);
+  }
+  $('[data-scroll]').on('click', scrollToSection);
+  // scroll button end
+
+
+
+
+
 
 
 
